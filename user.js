11e6a6846ba69b00a71dd1e5fe17cd3d08a1f7f1
vishnuchebol <119350787+vishnuchebol@users.noreply.github.com>
2023-04-24 15:46:40 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,56 @@
+[
+    {
+        "job":"Backend",
+        "country":"India",
+        "company":"Google",
+        "time":"Full time",
+        "overview":"A high paying job with limited work",
+        "requirements":"Good command over django and python",
+        "work_involved":"Creating backend part of apps"
+    },
+    {
+        "job":"Frontend",
+        "country":"US",
+        "company":"Apple",
+        "time":"Part Time",
+        "overview":"A high paying job with limited work",
+        "requirements":"Good command over django and python",
+        "work_involved":"Creating frontent part of apps"
+    },
+    {
+        "job":"App dev",
+        "country":"Australia",
+        "company":"Microsoft",
+        "time":"Full time",
+        "overview":"A high paying job with limited work",
+        "requirements":"Good command over django and python",
+        "work_involved":"Creating backend part of apps"
+    },
+    {
+        "job":"Cleaner",
+        "country":"China",
+        "company":"Facebook",
+        "time":"Full time",
+        "overview":"A high paying job with limited work",
+        "requirements":"Good command over django and python",
+        "work_involved":"Creating backend part of apps"
+    },
+    {
+        "job":"Teacher",
+        "country":"Australia",
+        "company":"Microsoft",
+        "time":"Full time",
+        "overview":"A high paying job with limited work",
+        "requirements":"Good command over django and python",
+        "work_involved":"Creating backend part of apps"
+    },
+    {
+        "job":"Software engineer",
+        "country":"Australia",
+        "company":"Microsoft",
+        "time":"Full time",
+        "overview":"A high paying job with limited work",
+        "requirements":"Good command over django and python",
+        "work_involved":"Creating backend part of apps"
+    }
+]
